7715cdefdd8fc22ab6e4ac00713d34c4a03ab4ef
Rahul Rajput <rahul242rajput@gmail.com>
2025-01-23 23:04:24 -0500

    Refactor layout structure and add new components for navigation, search, and user settings

---
+++ resources/js/Components/app/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="min-w-[200px]">
+        <div class="h-[80px] px-3 flex items-center gap-3">
+
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/app/SearchForm.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Search Form
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
